e685a1a5ec61b80fc4661f5eddab95d1bfc9d9e0
oait-h-m <oussamaaithmed1@gmail.com>
2025-02-28 02:06:08 +0000

    addd functions to check map is valid.

--- is_map_valid.c
+++ is_map_valid.c
@@ -12,30 +12,31 @@
 
 #include "so_long.h"
 
-int is_wall_valid(char **map, int row, int col)
+int	is_wall_valid(char **map, int row, int col)
 {
 	int i, (j);
 	i = 0;
 	while (map[i])
 	{
-		j = 0;	
+		j = 0;
 		while (map[i][j])
 		{
-			if ((i == 0 && map[i][j] != '1' && j < col - 1) || (i == (row - 1) && map[i][j] != '1' && j < col - 1))
-				return(0);
-			else if ((j == 0 && map[i][j] != '1') || (j == (col - 2) && map[i][j] != '1'))
-				return(0);
+			if ((i == 0 && map[i][j] != '1' && j < col - 1) || (i == (row - 1)
+					&& map[i][j] != '1' && j < col - 1))
+				return (0);
+			else if ((j == 0 && map[i][j] != '1') || (j == (col - 2)
+					&& map[i][j] != '1'))
+				return (0);
 			j++;
 		}
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
-int is_coin_valid(char **map)
+int	is_coin_valid(char **map)
 {
 	int i, (j), (coins);
-
 	coins = 0;
 	i = 0;
 	while (map[i])
@@ -49,13 +50,12 @@ int is_coin_valid(char **map)
 		}
 		i++;
 	}
-	return coins;
+	return (coins);
 }
 
-int is_exit_valid(char **map)
+int	is_exit_valid(char **map)
 {
 	int i, (j), (exit);
-
 	exit = 0;
 	i = 0;
 	while (map[i])
@@ -70,14 +70,13 @@ int is_exit_valid(char **map)
 		i++;
 	}
 	if (exit != 1)
-		return 0;
-	return exit;
+		return (0);
+	return (exit);
 }
 
-int is_player_valid(char **map)
+int	is_player_valid(char **map)
 {
 	int i, (j), (player);
-
 	player = 0;
 	i = 0;
 	while (map[i])
@@ -92,19 +91,21 @@ int is_player_valid(char **map)
 		i++;
 	}
 	if (player != 1)
-		return 0;
-	return player;
+		return (0);
+	return (player);
 }
 
-int is_map_valid(char **map, int row, int col)
+int	is_map_valid(char **map, int row, int col, char *av)
 {
-	if (!is_wall_valid(map, row, col))
+	if (!check_name_of_arg(av))
+		return 0;
+	else if (!is_wall_valid(map, row, col))
 		return (0);
 	else if (!is_coin_valid(map))
-			return 0;
+		return (0);
 	else if (!is_exit_valid(map))
-			return 0;
+		return (0);
 	else if (!is_player_valid(map))
-			return 0;
-	return 1;
+		return (0);
+	return (1);
 }
