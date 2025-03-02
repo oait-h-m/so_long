85ca2930afabe95f5532a04108f9507df2e719bb
oait-h-m <oussamaaithmed1@gmail.com>
2025-03-02 01:23:24 +0000

    fix header file.

--- so_long.h
+++ so_long.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG
-# define SO_LONG
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -19,37 +19,6 @@
 
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
-# include <fcntl.h>
-# include <limits.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct c_map
-{
-	int	row;
-	int	col;
-}		t_map;
-
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, const char *s2);
-char	*upd_strjoin(char *s1, char *s2);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen(const char *s);
-char	*ft_strcpy(char *s1, char *s2);
-
-int		is_wall_valid(char **map, int row, int col);
-int		is_coin_valid(char **map);
-int		is_exit_valid(char **map);
-int		is_player_valid(char **map);
-int		is_map_valid(char **map, int row, int col, char *av);
-int		check_name_of_arg(char *av);
-int		ft_strcmp(char *s1, char *s2);
-void	free_map(char **map);
-
-void	flood_fill(char **map, int x, int y);
-char	**map_copy(char **map, int rows);
-void	find_player(char **map, int *rows, int *columns);
-int		check_map_after_flood_fill(char **map);
+# include "src/src.h"
 
 #endif
