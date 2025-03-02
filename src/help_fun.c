6267f35f9d906a050e4e70692557f8955ed61ee8
oait-h-m <oussamaaithmed1@gmail.com>
2025-03-02 01:25:15 +0000

    add functions for help in my program.

---
+++ src/help_fun.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   help_fun.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 21:27:47 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/03/01 21:30:43 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "src.h"
+
+void	free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	if (!map || !*map)
+		return ;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
+char	**add_map_to_double_array(char **map, int fd, int row, int col)
+{
+	char	*s;
+
+	int i, (j);
+	i = 0;
+	while (i < row)
+	{
+		j = 0;
+		map[i] = malloc(sizeof(char) * col + 1);
+		if (!map[i])
+			return (NULL);
+		s = get_next_line(fd);
+		while (j < col)
+		{
+			map[i][j] = s[j];
+			j++;
+		}
+		map[i][j] = '\0';
+		i++;
+		free(s);
+	}
+	map[i] = NULL;
+	return (map);
+}
+
+char	**add_map_to_string(int row, int col, char *file_d)
+{
+	int		fd;
+	char	**map;
+
+	fd = open(file_d, O_RDONLY);
+	map = malloc(sizeof(char *) * (row + 1));
+	if (!map)
+		return (NULL);
+	add_map_to_double_array(map, fd, row, col);
+	return (map);
+}
+
+int	check_name_of_arg(char *av)
+{
+	if (ft_strlen(av) < 4)
+		return (0);
+	while (*av != '.')
+		av++;
+	if (ft_strcmp(av, ".ber") != 0)
+		return (0);
+	return (1);
+}
