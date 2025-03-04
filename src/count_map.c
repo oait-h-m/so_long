432edd3aca6b998d0bbe93e6dae550530410d103
oait-h-m <oussamaaithmed1@gmail.com>
2025-03-04 04:37:18 +0000

    update count map.

--- src/count_map.c
+++ src/count_map.c
@@ -41,7 +41,7 @@ int	count_line(char *file_d, int *len)
 		}
 		if (count_char(s, *len) == -1)
 		{
-			write(2, "Error\n", 6);
+			write(1, "Error\nMap is a rectangle\n", 23);
 			exit(1);
 		}
 		free(s);
