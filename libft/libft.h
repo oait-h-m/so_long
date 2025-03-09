b59c694d94f75d2ce8f46e49d139f87c1cb16593
oait-h-m <oussamaaithmed1@gmail.com>
2025-03-09 20:15:22 +0000

    add header file for libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,17 @@
 /*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 02:42:29 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/02/27 02:44:40 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/03/09 00:12:48 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-char	**ft_split(char const *s, char c);
+#include <unistd.h>
+
 int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr(int nb);
 
 #endif
